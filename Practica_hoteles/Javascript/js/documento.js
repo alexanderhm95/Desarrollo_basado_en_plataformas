d0713ea72d9bbf62187c12089bdd7f1029892485
alexanderhm95 <74020380+alexanderhm95@users.noreply.github.com>
2020-11-24 13:20:59 -0500

    javascript

---
+++ Practica_hoteles/Javascript/js/documento.js
@@ -0,0 +1,23 @@
+function saludar(){
+    alert("hola mundo")
+}
+function sumar(){
+    numero1 = parseInt(document.getElementById("n1").value);
+    numero2 = parseInt(document.getElementById("n2").value);
+    resultado = numero1 + numero2;
+    alert("La suma es: " + resultado);
+}
+function invertir(){
+    var cadenaObtenida = document.getElementById("cadena").innerText;
+    var vector = cadenaObtenida.split(''); // arreglo de caracteres
+    var reversa = vector.reverse(); // invertir el arreglo
+    var cadenInvertida = reversa.join('');  // une el arreglo en una sola cadena
+    document.write(cadenInvertida);
+}
+
+function validarFomulario(){
+    var nombres = document.getElementById("nombres");
+    if(nombres == ""){
+        alert("Por favor dígite el usuario");
+    }
+}
